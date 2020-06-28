d9fd26c9f5c7b5c3fc5a19660b0b840050fc6cd9
fido2020 <fidojj@hotmail.com>
2020-06-28 17:50:24 +1000

    build scripts and cat

---
+++ utils/cat.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <vector>
+
+std::vector<FILE*> files;
+
+int main(int argc, char** argv){
+    if(argc < 2){
+        files.push_back(stdin);
+    } else {
+        for(int i = 1; i < argc; i++){
+            if(strcmp(argv[i], "-") == 0){
+                files.push_back(stdin);
+                continue;
+            }
+
+            FILE* f = fopen(argv[i], "r");
+
+            if(!f){
+                fprintf(stderr, "%s: %s: %s\n", argv[i], strerror(errno));
+                continue;
+            }
+
+            files.push_back(f);
+        }
+    }
+
+    for(FILE*& f : files){
+        while(char c = fgetc(f)){
+            if(feof(f)){
+                break;
+            }
+
+            fputc(c, stdout);
+        }
+    }
+
+    return 0;
+}
